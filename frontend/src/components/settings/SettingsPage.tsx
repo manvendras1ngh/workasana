d7778e69795910d9f5e282908d8137d9b02b3365
Manvendra Vikram Singh <manvendrasinghrajput@outlook.com>
2026-01-29 15:53:19 +0530

    chore: update settings page

---
+++ frontend/src/components/settings/SettingsPage.tsx
@@ -0,0 +1,7 @@
+export const SettingsPage = () => {
+  return (
+    <div className="p-6 flex justify-center mt-22">
+      <p className="bg-sky-100 px-6 py-1 rounded-2xl">Coming Soon</p>
+    </div>
+  );
+};
