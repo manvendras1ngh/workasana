6b1aa8c594944daf265a713f3498110aaf078d76
Manvendra Vikram Singh <manvendrasinghrajput@outlook.com>
2026-01-29 15:32:43 +0530

    chore: add cookie parser library and update routes in index

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,15 @@
 import express from "express";
 import cors from "cors";
+import cookieParser from "cookie-parser";
 import { configDotenv } from "dotenv";
 import { instantiateConnection } from "./db/db.connect.js";
 import authRoutes from "./routes/auth.routes.js";
 import taskRoutes from "./routes/task.routes.js";
 import teamRoutes from "./routes/team.routes.js";
 import projectRoutes from "./routes/project.routes.js";
+import userRoutes from "./routes/user.routes.js";
+import tagRoutes from "./routes/tag.routes.js";
+import reportRoutes from "./routes/report.routes.js";
 
 //server config
 configDotenv({ path: "./.env" });
@@ -13,6 +17,7 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
+app.use(cookieParser());
 
 //db connection
 try {
@@ -27,7 +32,7 @@ app.use(
   cors({
     origin: process.env.CORS_ORIGIN,
     credentials: true,
-  })
+  }),
 );
 
 //api's
@@ -39,6 +44,9 @@ app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/tasks", taskRoutes);
 app.use("/api/v1/teams", teamRoutes);
 app.use("/api/v1/projects", projectRoutes);
+app.use("/api/v1/users", userRoutes);
+app.use("/api/v1/tags", tagRoutes);
+app.use("/api/v1/report", reportRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server running on port: http://localhost:${PORT}`);
