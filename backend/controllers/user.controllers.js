ecab969d628ca879f3672a007f6efb04ddabecc1
Manvendra Vikram Singh <manvendrasinghrajput@outlook.com>
2026-02-02 06:10:47 +0530

    feat: add user controller and routes to get all users in db.

---
+++ backend/controllers/user.controllers.js
@@ -0,0 +1,10 @@
+import asyncWrapper from "../utils/asyncWrapper.js";
+import { User } from "../models/User.models.js";
+
+export const getUsers = asyncWrapper(async (req, res) => {
+  const users = await User.find().select("-password").lean();
+
+  if (!users.length) return res.status(400).json({ error: "No users found" });
+
+  return res.status(200).json({ message: "All users", data: users });
+});
